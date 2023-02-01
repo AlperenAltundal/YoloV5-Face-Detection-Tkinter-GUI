b6cdea0daeec08799de8eadb3206865a157f36c8
Ahmet Alperen ALTUNDAL <99531967+AlperenAltundal@users.noreply.github.com>
2023-02-01 23:22:38 +0300

    Update and rename Face_Detect2.py to Face_Detect.py

--- Face_Detect.py
+++ Face_Detect.py
@@ -6,7 +6,6 @@ import imutils
 import matplotlib.pyplot as plt
 import threading
 import numpy as np
-# import SokukKod as Sk
 
 
 class FaceDetector:   
@@ -31,8 +30,7 @@ class FaceDetector:
             model = torch.hub.load("C:/Users/pc/yolov5-master/data", 'custom', 
             path='best_face_2.pt', source='local')    
         return model
-
-
+    
     counter=0
     count=0
     cx1=200
@@ -52,18 +50,15 @@ class FaceDetector:
     
 
     def plot_boxes(self, results, frame,TouchSayac):  
-        # print("fonk çalıştı")
+        
         #düz çizgi
         BoundCizgi=cv2.line(frame,(275,250),(275,450),(0,250,246),2)
         #kare
         kare = cv2.rectangle(frame, (175,250), (375,450), (0,191,255), 2)
 
-
-        # Z ekseni için point olusturalım 
         Z_ekseni=cv2.circle(frame, (1000,250), 5, (0,0,255), 10) 
         
         CounterTouch=0
-        
        
         labels, cord = results
         
@@ -73,23 +68,18 @@ class FaceDetector:
             row = cord[i]
             if row[4] >= 0.7:
                 
-
                 x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape),int(row[2]*x_shape), int(row[3]*y_shape)
 
                 bgr = (0, 255, 0)
                 sayi = float(row[4])
                 cv2.rectangle(frame, (x1, y1), (x2, y2), bgr,2)
-             
-                
 
                 rectx1, recty1 = ((x1+x2)/2, (y1+y2)/2)
                 rectcenter = int(rectx1),int(recty1)
                 cx=rectcenter[0]
                 cy=rectcenter[1]
                 cv2.circle(frame,(cx,cy),5,(0,255,0),-1)
-
-
-                
+          
                 KeyPoint=cx,cy
                 
                 if (cx >=270) and (cx <= 280) and (cy >=250) and(cy <= 450): 
@@ -98,32 +88,22 @@ class FaceDetector:
                     cv2.putText(frame,'degdi',(210,150),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255),2,cv2.LINE_AA)
                     TouchSayac+=1
                     print(TouchSayac) 
-                                
-                    # TouchCount1+=1
-                    # print(TouchCount1) 
 
-              
-
-
-                cv2.putText(frame, self.class_to_label(labels[i])+'    '+  str(KeyPoint) + '    '+ str(round(sayi, 2)),(x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)#+ Mesafe + "   "+
-
+                cv2.putText(frame, self.class_to_label(labels[i])+'    '+  str(KeyPoint) + '    '+ str(round(sayi, 2)),
+                            (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
 
 
         result = dict()
         result['frm'] = frame
         result['count'] = TouchSayac
 
-
         return result
-
-    
 
     def __call__(self):                 
         cap = self.get_video_capture()
         assert cap.isOpened()
         cx1=200
-   
-      
+    
         TouchCount1=0
         
 
@@ -132,15 +112,13 @@ class FaceDetector:
             ret, frame = cap.read()
             assert ret
 
-            frame = cv2.resize(frame, (1280, 720))     # video screen size
+            frame = cv2.resize(frame, (1280, 720))    
             
             start_time = time()                   
             results = self.score_frame(frame)
             result = self.plot_boxes(results, frame,TouchCount1)
             frame=result["frm"]
             TouchCount1=result["count"]
-            # TouchCount1+=1
-           
             
             end_time = time()
             fps = 1/np.round(end_time - start_time, 2)
@@ -153,11 +131,6 @@ class FaceDetector:
                     
         cap.release()                  
         cv2.destroyAllWindows()   
-           
-# class DetectionData:
-#     def __init__(self):
-#         self.frame
-#         self.TouchCount 
 
 
 alperen = 'yolov5s.pt'
