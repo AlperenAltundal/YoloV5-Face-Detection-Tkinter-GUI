a9afdfd9943c431f92c74b075b0ac79e663da296
Ahmet Alperen ALTUNDAL <99531967+AlperenAltundal@users.noreply.github.com>
2023-02-01 23:25:49 +0300

    Update and rename deneme1.py to Tkinter_UI.py

--- Tkinter_UI.py
+++ Tkinter_UI.py
@@ -8,17 +8,12 @@ import time
 from tkVideoPlayer import TkinterVideo
 import asyncio
 import threading
-# from Face_Detect2 import *
-# # from threading import Thread
-# import concurrent.futures
-# from concurrent.futures import ProcessPoolExecutor
 
-# import amınaKODUM as amk / enner valencia
+# from enner valencia import fenerbahçe
 
 import pandas as pd
 import asynctkinter as at
-# import tracemalloc
-# tracemalloc.start()
+
 
 
 
@@ -31,9 +26,7 @@ PastelMavi="#82AAE3"
 KoyuMavi="#144272"
 TozPembe="#947EC3"
 KahveRengi="#AA8B56"
-KapaliMavi="#5F7161"  # /#3C6255 /22A39F/439A97/5F8D4E/5F7161
-
-
+KapaliMavi="#5F7161"
 
 
 process=False
@@ -46,24 +39,6 @@ root.config(bg=KapaliMavi)
 output_widget = tk.Text(root)
 output_widget.config(width=78,height=11,bg=KoyuMavi,fg="white",font="Calibri 13")
 output_widget.place(x=25,y=450)
-
-
-# async def run_script():
-#     process = await asyncio.create_subprocess_exec(
-#         "python", Filepath, stdout=asyncio.subprocess.PIPE,
-#         stderr=asyncio.subprocess.PIPE)
-
-#     stdout, stderr = await process.communicate()
-#     output_widget.config(state='normal')
-#     output_widget.insert('end', stdout.decode('utf-8'))
-#     output_widget.config(state='disabled')
-    
-
-
-# def baslat():
-#     asyncio.run(run_script())
-
-
 
 
 def run_script():
@@ -80,26 +55,30 @@ def run_script():
         
         for line in HizAyari:
             print(line)
-            time.sleep(0.005)   #time.sleep(0.0001)
-
-# videoplayer=TkinterVideo(master=root,scaled=True)
-# videoplayer.load(process)
-# videoplayer.pack(expand=True)
-# videoplayer.play()
+            time.sleep(0.005)   
 
 
 
 
+def stop_script():
+    if not process:
+                    message_box=messagebox.showinfo(ErrorCode_1,"Stop fonksiyonu, start fonksiyonu başlamadan çağrılamaz")
+                    print(message_box)
+    else:
+                    process.kill()
+
+stop_btn=PhotoImage(file=StopButtonPath)
+img_label2=Label(image=stop_btn)
 
 
 
-
-
-
+IkonPhoto=PhotoImage(file="C:/Users/pc/vs_code/Z16_Face_Detect_2/resim3.png")
+root.iconphoto(False,IkonPhoto)
 
 
 start_btn=PhotoImage(file=StartButtonPath)
 img_label=Label(image=start_btn)
+
 
 start = tk.Button(  root,
                     image=start_btn,
@@ -112,40 +91,16 @@ start = tk.Button(  root,
 start.place(x=25,y=200)
 
 
-
-
-
-def stop_script():
-    if not process:
-                    message_box=messagebox.showinfo(ErrorCode_1,"Stop fonksiyonu, start fonksiyonu başlamadan çağrılamaz")
-                    print(message_box)
-    else:
-                    process.kill()
-
-
-stop_btn=PhotoImage(file=StopButtonPath)
-img_label2=Label(image=stop_btn)
-
-
-
-
-
 stop = tk.Button( 
-                  root,
-                  text="Face Detect  KAPA ",
-                  activebackground="red",
-                  image=stop_btn,
-                  activeforeground="black",
-                  command=stop_script,
-                  borderwidth=0
+                      root,
+                      text="Face Detect  KAPA ",
+                      activebackground="red",
+                      image=stop_btn,
+                      activeforeground="black",
+                      command=stop_script,
+                      borderwidth=0
                 )
 stop.place(x=25,y=300)
-
-
-
-IkonPhoto=PhotoImage(file="C:/Users/pc/vs_code/Z16_Face_Detect_2/resim3.png")
-root.iconphoto(False,IkonPhoto)
-
 
 
 BilgilendirmeLabel=tk.Label(
@@ -170,8 +125,6 @@ LogoLabel=tk.Label(
 LogoLabel.place(x=300,y=200)
             
 
-
-
 ConsoleOutput="key point coordinate information"
 ConsoleLabel=tk.Label(
                     root,
@@ -183,9 +136,6 @@ ConsoleLabel=tk.Label(
                     wraplength=350
                     )
 ConsoleLabel.place(x=25,y=418)
-
-
-#videolabel
 
 
 
